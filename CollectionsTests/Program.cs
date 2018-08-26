992c31814c16a2885d0b8e3c2e73bd728d272575
Denis Andrienko <e1895226@cmaisonneuve.qc.ca>
2018-08-26 17:42:27 -0400

    dictionary was created for test

--- CollectionsTests/Program.cs
+++ CollectionsTests/Program.cs
@@ -10,6 +10,33 @@ namespace CollectionsTests
     {
         static void Main(string[] args)
         {
+            Dictionary<string, string> catalog = new Dictionary<string, string>()
+            {
+                ["Jerome Hugon"] = "C#7. Developpez des aplications",
+                ["Paola Zannele"] = "Architecture et technologie des ordinateurs"
+            };
+
+
+            catalog.Add("Yves Ligter", "Cours preparation A+");
+            ShowCatalog();
+            void ShowCatalog()
+            {
+                foreach (KeyValuePair<string, string> pair in catalog)
+                {
+                    Console.WriteLine($" author: {pair.Key} , name of book: {pair.Value}");
+                }
+             
+            }
+            Console.ReadKey();
+
+            
+
+
+
         }
-    }
+      
+            
+            
+
+    }    
 }
