cc5771e03137102d3b2c6f3e9ef5d8a718dc4be8
Denis Andrienko <e1895226@cmaisonneuve.qc.ca>
2018-08-26 17:13:03 -0400

    Add project files.

---
+++ CollectionsTests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
